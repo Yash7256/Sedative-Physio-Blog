902d76bc88fb6c9723a432aeadfadf0e2cfcac9b
Yash7256 <amanrajengg@gmail.com>
2026-01-20 00:11:20 +0530

    Add .gitignore with proper exclusions and remove node_modules/.next from tracking

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/yash/Documents/sedative physio/src/app/page\":[\"static/media/90475aac776488b6-s.p.woff2\"],\"/home/yash/Documents/sedative physio/src/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\",\"static/media/90475aac776488b6-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
